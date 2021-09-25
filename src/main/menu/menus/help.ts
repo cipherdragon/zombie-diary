548637b5bd39fb16a8fd1221e4d3b442ccbb805b
Adeepa Gunathilake <adeepagunathilake@gmail.com>
2021-09-26 01:08:23 +0530

    Removed links tied to original author

--- src/main/menu/menus/help.ts
+++ src/main/menu/menus/help.ts
@@ -4,9 +4,7 @@ import { MenuItemConstructorOptions, shell } from "electron";
 
 import { translate } from "../../i18n/i18n";
 
-const URL_LICENSE = "https://github.com/samuelmeuli/mini-diary/blob/master/LICENSE.md";
-const URL_PRIVACY_POLICY = "https://github.com/samuelmeuli/mini-diary/blob/master/PRIVACY.md";
-const URL_WEBSITE = "https://minidiary.app";
+const URL_LICENSE = "https://github.com/cipherdragon/zombie-diary/blob/master/LICENSE.md"
 
 export default function getHelpMenu(): MenuItemConstructorOptions {
 	return {
@@ -14,22 +12,9 @@ export default function getHelpMenu(): MenuItemConstructorOptions {
 		role: "help",
 		submenu: [
 			{
-				label: translate("website"),
-				click(): void {
-					shell.openExternal(URL_WEBSITE);
-				},
-			},
-			{ type: "separator" },
-			{
 				label: translate("license"),
 				click(): void {
 					shell.openExternal(URL_LICENSE);
-				},
-			},
-			{
-				label: translate("privacy-policy"),
-				click(): void {
-					shell.openExternal(URL_PRIVACY_POLICY);
 				},
 			},
 		],
